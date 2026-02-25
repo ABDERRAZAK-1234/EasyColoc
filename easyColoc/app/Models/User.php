70c8eae1690926414bf16c4fc3b98ec85ff65c6e
Abderrazak <aamaichabderrazak2004@gmail.com>
2026-02-25 09:57:35 +0000

    J'ai ajouter les migrations des quelque tables

--- easyColoc/app/Models/User.php
+++ easyColoc/app/Models/User.php
@@ -12,6 +12,11 @@ class User extends Authenticatable
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
+    public function colocation()
+    {
+        return $this->belongsTo(Colocation::class);
+    }
+
     /**
      * The attributes that are mass assignable.
      *
